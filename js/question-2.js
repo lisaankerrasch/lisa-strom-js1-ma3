4a328dcf94d101c3ae90ad2f1e37b11a77977bef
lisaankerrasch <lisa.ar.strom@gmail.com>
2021-02-02 15:04:33 +0100

    added classes and styling

--- js/question-2.js
+++ js/question-2.js
@@ -1,30 +1,27 @@
 const url =
   "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating";
-const gamesContainer = document.querySelector(".games");
+const gamesContainer = document.querySelector(".container");
 
 async function getGames() {
-  //   try {
-  const response = await fetch(url);
-  const list = await response.json();
-  const game = list.results;
+  try {
+    const response = await fetch(url);
+    const list = await response.json();
+    const game = list.results;
 
-  gamesContainer.innerHTML = "";
+    gamesContainer.innerHTML = "";
 
-  for (let i = 0; i < game.length; i++) {
-    console.log(game[i].name);
-    console.log(game[i].rating);
-
-    if (i === 8) {
-      break;
-    }
-    gamesContainer.innerHTML += `<div class="games"><p class="name"> 
+    for (let i = 0; i < game.length; i++) {
+      if (i === 8) {
+        break;
+      }
+      gamesContainer.innerHTML += `<div class="games"><p class="name"> 
     ${game[i].name} <p>
     <p class="rating"> Rating: ${game[i].rating} </p>
     <p> Number of tags: ${game[i].tags.length} </p> </div>`;
+    }
+  } catch (error) {
+    gamesContainer.innerHTML += `<div class="error"> There has been an error </div>`;
   }
-  //   } catch (error) {
-  //     console.log("there has been an error");
-  //   }
 }
 
 getGames();
