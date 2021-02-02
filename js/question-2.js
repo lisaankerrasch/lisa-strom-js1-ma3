496e25980c2972a1e592f7efb79f73ade4d28250
lisaankerrasch <lisa.ar.strom@gmail.com>
2021-02-02 15:55:56 +0100

    fixed the loader

--- js/question-2.js
+++ js/question-2.js
@@ -1,6 +1,7 @@
 const url =
-  "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating";
+  "https://api.rawg.io/api/ames?dates=2019-01-01,2019-12-31&ordering=-rating";
 const gamesContainer = document.querySelector(".container");
+const loader = document.querySelector(".loader");
 
 async function getGames() {
   try {
@@ -8,19 +9,25 @@ async function getGames() {
     const list = await response.json();
     const game = list.results;
 
-    gamesContainer.innerHTML = "";
+    setTimeout(function () {
+      gamesContainer.innerHTML = "";
 
-    for (let i = 0; i < game.length; i++) {
-      if (i === 8) {
-        break;
-      }
-      gamesContainer.innerHTML += `<div class="games"><p class="name"> 
+      for (let i = 0; i < game.length; i++) {
+        if (i === 8) {
+          break;
+        }
+
+        gamesContainer.innerHTML += `<div class="games"><p class="name"> 
     ${game[i].name} <p>
     <p class="rating"> Rating: ${game[i].rating} </p>
     <p> Number of tags: ${game[i].tags.length} </p> </div>`;
-    }
+      }
+    }, 2000);
   } catch (error) {
-    gamesContainer.innerHTML += `<div class="error"> There has been an error </div>`;
+    setTimeout(function () {
+      loader.classList.remove("loader");
+      gamesContainer.innerHTML += `<div class="error"> There has been an error </div>`;
+    }, 2500);
   }
 }
 
